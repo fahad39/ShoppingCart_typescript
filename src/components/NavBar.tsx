f2a461f5be8743a28d5b7571ec8703794873b842
fahad39 <fahadhussain0127@gmail.com>
2023-07-20 20:22:29 +0500

    added nav bar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,9 @@
+import { Navbar as NavbarBs, Container } from "react-bootstrap";
+
+export function NavBar() {
+  return (
+    <NavbarBs>
+      <Container>NAv</Container>
+    </NavbarBs>
+  );
+}
