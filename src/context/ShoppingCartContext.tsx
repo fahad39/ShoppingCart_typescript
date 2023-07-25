54c06f9326b288abf1f4a9891fbcca3ed0ced5e0
fahad39 <fahadhussain0127@gmail.com>
2023-07-25 19:06:25 +0500

    added cart and local storage functionality

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -1,4 +1,6 @@
 import { createContext, useContext, ReactNode, useState } from "react";
+import { ShoppingCart } from "../components/ShoppingCart";
+import { useLocalStorage } from "../hooks/useLocalStorage";
 
 type ShoppinCartProviderProps = {
   children: ReactNode;
@@ -14,6 +16,10 @@ type ShoppingCartContext = {
   increaseCartQuantity: (id: number) => void;
   decreaseCartQuantity: (id: number) => void;
   removeFromCart: (id: number) => void;
+  openCart: () => void;
+  closeCart: () => void;
+  cartQuantity: number;
+  cartItems: CartItem[];
 };
 
 const ShoppinCartContext = createContext({} as ShoppingCartContext);
@@ -23,7 +29,11 @@ export function useShoppingCart() {
 }
 
 export function ShoppingCartProvider({ children }: ShoppinCartProviderProps) {
-  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+  const [cartItems, setCartItems] = useLocalStorage<CartItem[]>(
+    "shopping-cart",
+    []
+  );
+  const [isOpen, setIsOpen] = useState(false);
 
   function getItemQuantity(id: number) {
     return cartItems.find((item) => item.id === id)?.quantity || 0;
@@ -48,7 +58,7 @@ export function ShoppingCartProvider({ children }: ShoppinCartProviderProps) {
   function decreaseCartQuantity(id: number) {
     setCartItems((currItems) => {
       if (currItems.find((item) => item.id === id)?.quantity === 1) {
-        return currItems.filter((item) => item.id == id);
+        return currItems.filter((item) => item.id !== id);
       } else {
         return currItems.map((item) => {
           if (item.id === id) {
@@ -67,6 +77,14 @@ export function ShoppingCartProvider({ children }: ShoppinCartProviderProps) {
     });
   }
 
+  const cartQuantity = cartItems.reduce(
+    (quantity, item) => item.quantity + quantity,
+    0
+  );
+
+  const openCart = () => setIsOpen(true);
+  const closeCart = () => setIsOpen(false);
+
   return (
     <ShoppinCartContext.Provider
       value={{
@@ -74,9 +92,14 @@ export function ShoppingCartProvider({ children }: ShoppinCartProviderProps) {
         increaseCartQuantity,
         decreaseCartQuantity,
         removeFromCart,
+        cartItems,
+        cartQuantity,
+        openCart,
+        closeCart,
       }}
     >
       {children}
+      <ShoppingCart isOpen={isOpen} />
     </ShoppinCartContext.Provider>
   );
 }
